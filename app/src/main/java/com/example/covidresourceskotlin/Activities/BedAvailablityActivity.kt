d005bdead5cfd9db1cf713a8c22cedac9c722376
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-05-08 16:38:25 +0530

    Updated layout of PostActivity.kt

--- app/src/main/java/com/example/covidresourceskotlin/Activities/BedAvailablityActivity.kt
+++ app/src/main/java/com/example/covidresourceskotlin/Activities/BedAvailablityActivity.kt
@@ -1,5 +1,6 @@
 package com.example.covidresourceskotlin.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,7 +14,12 @@ class BedAvailablityActivity : AppCompatActivity() {
 
         addResource.setOnClickListener(View.OnClickListener {
 
+            val intent = Intent(this,PostActivity::class.java)
+            startActivity(intent)
+
         })
+
+
 
 
     }
--- app/src/main/java/com/example/covidresourceskotlin/Activities/PostActivity.kt
+++ app/src/main/java/com/example/covidresourceskotlin/Activities/PostActivity.kt
@@ -2,14 +2,30 @@ package com.example.covidresourceskotlin.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.covidresourceskotlin.R
+import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
 
+        val cities = resources.getStringArray(R.array.cities)
+        val arrayAdapter = ArrayAdapter(this, R.layout.drop_down_item, cities)
+        autoComplete.setAdapter(arrayAdapter)
 
+        post.setOnClickListener{
+            Toast.makeText(this,autoComplete.text,Toast.LENGTH_SHORT).show()
+        }
+
+        addImage.setOnClickListener{
+
+
+
+
+        }
 
     }
 }
